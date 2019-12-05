83835c9e4b3af49fd66fcf1a09c16ad5762e9fd1
jBosak98 <jbosak98@gmail.com>
2019-12-05 13:20:59 +0100

    backend init

---
+++ backend/forms.py
@@ -0,0 +1,21 @@
+
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', [validators.Length(min=4, max=25, message="zjebales")])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    remember_me = BooleanField('Remember Me')
+
+    submit = SubmitField('Sign In')
+
+
+
